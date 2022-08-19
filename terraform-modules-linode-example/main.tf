68a914f79916944f9917b2325bf082ce2b743243
teckwei <techong@akamai.com>
2022-08-19 15:33:36 +0800

    exclude the ppt file

--- terraform-modules-linode-example/main.tf
+++ terraform-modules-linode-example/main.tf
@@ -17,7 +17,7 @@ module "dev" {
 # Variables Specific to this Deployment
   root_pass = var.root_pass
 
-  # Variables Specific to Servers
+  # Variables Specific to Servers and database cluster
   webserver_label = "client-dev-web"
   dbserver_label = "client-dev-db"
 
@@ -30,10 +30,10 @@ module "dev" {
 module "staging" {
   source = "./modules/staging"
 
-# Variables Specific to this Deployment
+  # Variables Specific to this Deployment
   root_pass = var.root_pass
 
-  # Variables Specific to Servers
+  # Variables Specific to Servers and database cluster
   webserver_label = "client-staging-web"
   dbserver_label = "client-staging-db"
 
@@ -49,7 +49,7 @@ module "prod" {
 # Variables Specific to this Deployment
   root_pass = var.root_pass
 
-  # Variables Specific to Servers
+  # Variables Specific to Servers and database cluster
   webserver_label = "client-prod-web"
   dbserver_label = "client-prod-db"
 
