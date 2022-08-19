5eca0d8e0bd3732ebb986bf700c1920d2c8bd4a0
teckwei <techong@akamai.com>
2022-08-19 14:00:00 +0800

    add production moduleon terraform-modules-linode-example

--- terraform-modules-linode-example/main.tf
+++ terraform-modules-linode-example/main.tf
@@ -18,8 +18,8 @@ module "dev" {
   root_pass = var.root_pass
 
   # Variables Specific to Servers
-  webserver_label = "client1-web"
-  dbserver_label = "client1-db"
+  webserver_label = "client-dev-web"
+  dbserver_label = "client-dev-db"
 
   engine_id = var.engine_id
   region = var.region
@@ -34,8 +34,24 @@ module "staging" {
   root_pass = var.root_pass
 
   # Variables Specific to Servers
-  webserver_label = "client2-web"
-  dbserver_label = "client2-db"
+  webserver_label = "client-staging-web"
+  dbserver_label = "client-staging-db"
+
+  engine_id = var.engine_id
+  region = var.region
+  type = var.type
+  image = var.image
+}
+
+module "prod" {
+  source = "./modules/prod"
+
+# Variables Specific to this Deployment
+  root_pass = var.root_pass
+
+  # Variables Specific to Servers
+  webserver_label = "client-prod-web"
+  dbserver_label = "client-prod-db"
 
   engine_id = var.engine_id
   region = var.region
